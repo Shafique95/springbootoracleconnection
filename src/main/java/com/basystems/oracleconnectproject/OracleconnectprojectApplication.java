4c32f9a610e625da9fbb7f0f21e2c084924edfad
Shafiqul Islam <www.shafiquebd@gmail.com>
2024-12-04 09:36:26 +0600

    initialize project

---
+++ src/main/java/com/basystems/oracleconnectproject/OracleconnectprojectApplication.java
@@ -0,0 +1,13 @@
+package com.basystems.oracleconnectproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleconnectprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleconnectprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basystems/oracleconnectproject/OracleconnectprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basystems.oracleconnectproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleconnectprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
